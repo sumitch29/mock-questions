ad2edf904a5cd4bf5a976e87fe65d82438859264
Sumit Chavhan <srchavhan29@gmail.com>
2021-11-29 14:39:23 +0530

    question6

---
+++ Question6/app.js
@@ -0,0 +1,21 @@
+const inPut = document.querySelector("#input");
+const butTon = document.querySelector("#button");
+const outPut = document.querySelector("#output");
+
+butTon.addEventListener('click',clickHandler)
+
+function clickHandler(){
+    let inputValue = inPut.value
+    let squareValue = (inPut.value)**2
+    console.log(squareValue)
+    let autoMorphic = squareValue % 10
+    let checkNumber = autoMorphic
+    console.log(autoMorphic)
+    console.log(checkNumber)
+    console.log(inputValue)
+    if(inPut == checkNumber){
+            outPut.innerText = "number is automorphic"
+    }else{
+        outPut.innerText = "number is not automorphic"
+    }
+}
---
+++ Question6/readme.md
@@ -0,0 +1,9 @@
+Coding: Input a single-digit number and check whether the number is an Automorphic number or not. Render output on the page.
+An automorphic number is a number whose square "ends" in the same digits as the number itself.
+For example,
+52 = 25, here 25 ends with ‘5’. Display :"5 is an Automorphic Number" on the screen
+62 = 36, here 36 ends with ‘6’
+So 5 and 6 are automorphic numbers
+
+7 isn't an automorphic number because 7**2=49.Display :"7 is not an Automorphic Number" on the screen
+(done by using  Vanilla JS on Vscode)
