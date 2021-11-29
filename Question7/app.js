8e3afe0746ce827a7a756c46dc31f7da8fcdbde5
Sumit Chavhan <srchavhan29@gmail.com>
2021-11-29 15:04:39 +0530

    question7

---
+++ Question7/app.js
@@ -0,0 +1,27 @@
+const butTon = document.querySelector("#button");
+const outPut = document.querySelector("#output");
+let array =[
+    {
+        task : "playing cricket",
+        completed : true,
+    },
+    {
+        task : "playing cricket",
+        completed : false,
+    },
+    {
+        task : "playing cricket",
+        completed : true,
+    },
+    {
+        task : "playing cricket",
+        completed : true,
+    }
+]
+
+butTon.addEventListener('click',()=>{
+        for(let i=0 ; i<array.length ; i++)
+        if(array[i].completed){
+            outPut.innerHTML += `<p>${array[i].task}</p>`
+
+}})
